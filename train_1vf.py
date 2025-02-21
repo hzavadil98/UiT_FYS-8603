457ea5781b4ef60ad71214ea4e7de107cb25889e
Jan Zavadil <hzavadil98@gmail.com>
2025-02-21 15:45:00 +0100

    1vf finally workink - propper training

--- train_1vf.py
+++ train_1vf.py
@@ -29,7 +29,7 @@ def main():
             root_folder=root_folder,
             annotation_csv="modified_breast-level_annotations.csv",
             imagefolder_path="New_512",
-            batch_size=32,
+            batch_size=64,
             num_workers=8,
             view=i,
         )
@@ -59,14 +59,14 @@ def main():
         # figure out if running with mps or gpu or cpu
 
         trainer = pl.Trainer(
-            max_epochs=3,
+            max_epochs=100,
             accelerator=accelerator,
             devices=devices,
             logger=wandb_logger,
             callbacks=[checkpoint_callback, lr_monitor, early_stopping],
-            limit_train_batches=3,  # Only 5 training batches per epoch
-            limit_val_batches=2,
-            log_every_n_steps=1,
+            # limit_train_batches=3,  # Only 5 training batches per epoch
+            # limit_val_batches=2,
+            # log_every_n_steps=1,
         )
 
         # Train
