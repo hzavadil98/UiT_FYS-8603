23597e070ff9854704e5b74b0a79d8ec424c4b21
Jan Zavadil <hzavadil98@gmail.com>
2025-02-24 17:13:55 +0100

    trying training with a lot of transforms, but still only on one view per featurizer

--- train_1vf.py
+++ train_1vf.py
@@ -99,6 +99,7 @@ def main():
             logger=wandb_logger,
             callbacks=[checkpoint_callback, lr_monitor, early_stopping],
             log_every_n_steps=5,
+            strategy="ddp",
             # limit_train_batches=3,  # Only 5 training batches per epoch
             # limit_val_batches=2,
             # log_every_n_steps=1,
