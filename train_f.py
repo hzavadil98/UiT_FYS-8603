61a4a08543c68fa989bba21b9bc4d109d3fcaaaa
Jan Zavadil <hzavadil98@gmail.com>
2025-02-25 10:38:19 +0100

    trying training with a lot of transforms, but still only on one view per featurizer

--- train_f.py
+++ train_f.py
@@ -60,7 +60,7 @@ def main():
         annotation_csv="modified_breast-level_annotations.csv",
         imagefolder_path="New_512",
         batch_size=32,
-        num_workers=16,
+        num_workers=8,
         train_transform=train_transform,
         transform=transform,
     )
@@ -88,17 +88,17 @@ def main():
     # figure out if running with mps or gpu or cpu
 
     trainer = pl.Trainer(
-        max_epochs=60,
+        max_epochs=10,
         accelerator=accelerator,
         devices=devices,
         logger=wandb_logger,
         callbacks=[checkpoint_callback, lr_monitor, early_stopping],
-        log_every_n_steps=5,
-        # limit_train_batches=3,  # Only 5 training batches per epoch
-        # limit_val_batches=2,
-        # log_every_n_steps=1,
+        log_every_n_steps=1,
+        limit_train_batches=3,  # Only 5 training batches per epoch
+        limit_val_batches=2,
     )
 
+    print("Starting training")
     # Train
     trainer.fit(model, dataloader)
     # Load best weights
