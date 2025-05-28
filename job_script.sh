5e58a0fc77aa9cd0b557a31e5f0409c259594786
Jan Zavadil <hzavadil98@gmail.com>
2025-05-28 17:24:41 +0200

    synt_task2

--- job_script.sh
+++ job_script.sh
@@ -2,13 +2,7 @@
 
 echo "Running the python script..."
 
-# Example of evaluating a Python expression:
-echo "Evaluating a Python expression (2 + 2):"
-python -c "print(2 + 2)"
 
-echo "Evaluating another Python expression (string concatenation):"
-python -c "print('Hello' + ' ' + 'World!')"
-
-python train_synt.py
-#python train_4v2b.py
+#python train_synt.py
+python train_4v2b.py
 #python train_2v1b.py
