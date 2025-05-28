278c881a86742d8b66565050c10b529abe892922
Jan Zavadil <hzavadil98@gmail.com>
2025-05-28 17:50:30 +0200

    synt_task2

---
+++ env_test.py
@@ -0,0 +1,6 @@
+import sys
+
+print("Python version via sys:", sys.version)
+print("Python executable via sys:", sys.executable)
+print("Python path via sys:", sys.path)
+print("Minimal Python script executed successfully!")
