f5ba08cd3d38dc4823d66ed33129df208908f366
Jan Zavadil <hzavadil98@gmail.com>
2025-06-02 13:20:22 +0200

    finally running adjusted transforms in dataloader

--- train_synt.py
+++ train_synt.py
@@ -73,7 +73,7 @@ def main():
     ##########################################################################################################
     model = TwoViewCNN(
         num_classes=3,
-        task=2,
+        task=1,
         num_views=2,
         input_channels=1,
         resnext_inplanes=16,
