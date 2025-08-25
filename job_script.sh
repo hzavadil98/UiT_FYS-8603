acc17f0ac3a57cbe62441b71b241fa9c810d3cda
Jan Zavadil <hzavadil98@gmail.com>
2025-08-25 20:37:46 +0200

    First training after recovery

--- job_script.sh
+++ job_script.sh
@@ -7,6 +7,6 @@ echo "Running the python script..."
 #python train_synt.py
 
 #python train_4v2b.py
-pip install imageio.v3
+pip install imageio
 python train_2v1b.py
 echo "Script execution completed."
