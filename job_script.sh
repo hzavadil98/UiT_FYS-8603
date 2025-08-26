cec855259a7cf03304d461af425c800399624be1
Jan Zavadil <hzavadil98@gmail.com>
2025-08-26 14:48:33 +0200

    Second training after recovery

--- job_script.sh
+++ job_script.sh
@@ -7,6 +7,8 @@ echo "Running the python script..."
 #python3 train_synt.py
 
 #python3 train_4v2b.py
+python3 --version
+uv --version
 
-python train_2v1b.py
+python3 train_2v1b.py
 echo "Script execution completed."
