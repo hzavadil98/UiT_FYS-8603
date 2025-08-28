ee0d466c59a148061a43055b0516eb73175f6f26
Jan Zavadil <hzavadil98@gmail.com>
2025-08-28 17:48:58 +0200

    train on resized images

--- job_script.sh
+++ job_script.sh
@@ -7,8 +7,6 @@ echo "Running the python script..."
 #python3 train_synt.py
 
 #python3 train_4v2b.py
-apt-get update
-apt-get install -y iputils-ping
-ping api.wandb.ai
+curl https://api.wandb.ai
 #python3 train_2v1b.py
 echo "Script execution completed."
