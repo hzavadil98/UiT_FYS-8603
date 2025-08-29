5b30282172c5757600dbb8462b3de9ce021c0b4d
Jan Zavadil <hzavadil98@gmail.com>
2025-08-29 09:51:56 +0200

    creating resized imgs

--- job_script.sh
+++ job_script.sh
@@ -8,5 +8,6 @@ echo "Running the python script..."
 
 #python3 train_4v2b.py
 
-python3 train_2v1b.py
+#python3 train_2v1b.py
+python3 resize_png.py
 echo "Script execution completed."
--- resize_png.py
+++ resize_png.py
@@ -1,14 +1,15 @@
 import os
 
 from PIL import Image
+from tqdm import tqdm
 
 input_folder = "/Users/jazav7774/Data/Mammo/images_png"
 output_folder = "/Users/jazav7774/Data/Mammo/images_png_396"
 os.makedirs(output_folder, exist_ok=True)
 
-target_size = (660, 396)
+target_size = (369, 660)
 
-for filename in os.listdir(input_folder):
+for filename in tqdm(os.listdir(input_folder)):
     if filename.endswith(".png"):
         img = Image.open(os.path.join(input_folder, filename))
         img = img.resize(
