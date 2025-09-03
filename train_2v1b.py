559c20be153ae50e73d046fcff42f83958959296
Jan Zavadil <hzavadil98@gmail.com>
2025-09-03 16:55:56 +0200

    Training synth data with shared structure

--- train_2v1b.py
+++ train_2v1b.py
@@ -63,9 +63,9 @@ def main():
     #    ]
     # )
 
-    imagefolder_path = "New_512"
-    image_format = "dicom"
-    norm_kind = "zscore"
+    imagefolder_path = "images_png_369"
+    image_format = "png"
+    norm_kind = "dataset_zscore"
     batch_size = 32
     task = 1  # 1 for cancer, 2 for density
 
