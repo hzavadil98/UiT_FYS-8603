70c7dae351534627cbc4678be02d1de7b70d792b
Jan Zavadil <hzavadil98@gmail.com>
2026-05-20 20:54:44 +0200

    retrain cancer model

--- train_1vf.py
+++ train_1vf.py
@@ -100,7 +100,7 @@ def main():
     early_stopping = EarlyStopping(monitor="val_loss", patience=8, mode="min")
 
     trainer = pl.Trainer(
-        max_epochs=5,
+        max_epochs=100,
         accelerator=accelerator,
         devices=devices,
         logger=wandb_logger,
